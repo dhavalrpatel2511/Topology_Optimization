6460507a3efed1f0391e995d0a438ddfeea63829
dhavalrpatel2511 <49568565+dhavalrpatel2511@users.noreply.github.com>
2023-06-15 12:23:42 +0200

    Add files via upload

---
+++ Plugin_TO/plugin_TO_plugin.py
@@ -0,0 +1,19 @@
+from abaqusGui import getAFXApp, Activator, AFXMode
+from abaqusConstants import ALL
+import os
+thisPath = os.path.abspath(__file__)
+thisDir = os.path.dirname(thisPath)
+
+toolset = getAFXApp().getAFXMainWindow().getPluginToolset()
+toolset.registerGuiMenuButton(
+    buttonText='Topology_Optimization', 
+    object=Activator(os.path.join(thisDir, 'plugin_TODB.py')),
+    kernelInitString='import Multi_Materials_TO',
+    messageId=AFXMode.ID_ACTIVATE,
+    icon=None,
+    applicableModules=ALL,
+    version='N/A',
+    author='N/A',
+    description='N/A',
+    helpUrl='N/A'
+)
